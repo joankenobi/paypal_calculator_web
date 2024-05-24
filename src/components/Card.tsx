69b75668ce37f54256eb07b1ea4d3a7b4809f929
joankenobi <joan1233blanco@gmail.com>
2024-05-24 07:56:08 -0400

    feat: reutilización de compionente CardBody

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,10 +5,22 @@ export function Card() {
         } // mala practica
         }>
             <div className="card-body">
-                <h5 className=" text-black ">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button className=" border-blue-800 bg-blue-700 p-2 text-white  rounded-lg border-2  ">Go somewhere</button>
+                <CardBody />
             </div>
         </div>
+    )
+}
+
+export function CardBody() {
+    return (
+        <>
+            <h5 className=" text-black ">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button className=" border-blue-800 bg-blue-700 p-2 text-white  rounded-lg border-2  ">
+                <a href="#">
+                    Go somewhere
+                </a>
+            </button>
+        </>
     )
 }
