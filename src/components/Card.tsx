584df5fbf6e190ecc1f8166c4c9ca9fbe692e056
joankenobi <joan1233blanco@gmail.com>
2024-05-23 21:43:01 -0400

    feat apply bad practices in card styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,9 @@
 export function Card() {
     return (
-        <div className=" bg-slate-200 border-black p-3 rounded-lg w-auto">
+        <div className=" bg-slate-200 border-black p-3 rounded-lg w-auto" style={{
+            width: '350px'
+        } // mala practica
+        }>
             <div className="card-body">
                 <h5 className=" text-black ">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
