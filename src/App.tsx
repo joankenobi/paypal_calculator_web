2030dd6fbbe6b6985646eea58d3915538aa5fb00
joankenobi <joan1233blanco@gmail.com>
2024-05-26 16:29:21 -0400

    feat: added card body

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import { BrowserRouter} from 'react-router-dom'
 import { Toaster } from 'react-hot-toast';
 import { CalculatorPage } from './pages/CalculatorPage';
 import { Navigation } from './components/Navigation';
-import { Card } from './components/Card';
+import { Card, CardBody } from './components/Card';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container mx-auto">
         <Navigation />
-        <Card />
+        <Card body={"hola mundo"} />
+        <CardBody 
+          title={"hola mundo"}
+          text={"hola mundo"}
+          button={{
+            text: "go to google",
+            link: "https://www.google.com"
+          }} 
+        
+        />
         <CalculatorPage />
         <Toaster /> {/* toaster muestra notificaciones pero no se ejecuta hasta ejecutar toast en una page*/}
       </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,42 @@
-export function Card() {
+interface CardProps {
+    body: string
+}
+
+export function Card(props:CardProps) {
+    const {body} = props
     return (
         <div className=" bg-slate-200 border-black p-3 rounded-lg w-auto" style={{
             width: '350px'
         } // mala practica
-        }>
+    }>
             <div className="card-body">
-                <CardBody />
+                <CardBody text="hola mundo" title="title" button={
+                    {
+                        text: "go to google",
+                        link: "https://www.google.com"
+                }} />
             </div>
         </div>
     )
 }
+interface CardBodyProps {
+    text: string
+    title: string
+    button:{
+        text:string
+        link?:string
+    }
+}
 
-export function CardBody() {
+export function CardBody( props:CardBodyProps) {
+    const {text,title,button} = props
     return (
         <>
-            <h5 className=" text-black ">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 className=" text-black ">{title}</h5>
+            <p className="card-text">{text}</p>
             <button className=" border-blue-800 bg-blue-700 p-2 text-white  rounded-lg border-2  ">
-                <a href="#">
-                    Go somewhere
+                <a href={button.link}>
+                    {button.text}
                 </a>
             </button>
         </>
